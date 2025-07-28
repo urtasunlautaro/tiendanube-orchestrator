446de23339bffb8fd3c1decfd7ca2b55f6ad77ec
urtasunlautaro <urtasunlautaro@gmail.com>
2025-07-28 20:22:05 -0300

    feat: models

---
+++ tiendanube-orchestrator/internal/models/models.go
@@ -0,0 +1,35 @@
+package models
+
+import "time"
+
+type TransactionRequest struct {
+	Value              string `json:"value"`
+	Description        string `json:"description"`
+	Method             string `json:"method"`
+	CardNumber         string `json:"cardNumber"`
+	CardHolderName     string `json:"cardHolderName"`
+	CardExpirationDate string `json:"cardExpirationDate"`
+	CardCVV            string `json:"cardCvv"`
+}
+
+type Transaction struct {
+	ID                 string `json:"id"`
+	Value              string `json:"value"`
+	Description        string `json:"description"`
+	Method             string `json:"method"`
+	CardNumber         string `json:"cardNumber"`
+	CardHolderName     string `json:"cardHolderName"`
+	CardExpirationDate string `json:"cardExpirationDate"`
+	CardCVV            string `json:"cardCvv"`
+}
+
+type Receivable struct {
+	ID            string    `json:"id"`
+	TransactionID string    `json:"transaction_id"`
+	Status        string    `json:"status"`
+	CreateDate    time.Time `json:"create_date"`
+	PaymentDate   time.Time `json:"payment_date"`
+	Subtotal      string    `json:"subtotal"`
+	Discount      string    `json:"discount"`
+	Total         string    `json:"total"`
+}
