3a939211acd0778e205e7d16f56f6d66a9afcef2
urtasunlautaro <urtasunlautaro@gmail.com>
2025-07-29 13:03:51 -0300

    feat: adds main

---
+++ tiendanube-orchestrator/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	h "github.com/urtasunlautaro/orchestrator/internal/handler"
+	p "github.com/urtasunlautaro/orchestrator/internal/processor"
+	s "github.com/urtasunlautaro/orchestrator/internal/services"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	dbService := s.NewDatabase(logger)
+	numService := s.NewNumerator(logger)
+	processor := p.NewProcessor(dbService, numService, logger)
+	handler := h.NewOrchestratorHandler(processor, logger)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /transactions", handler.CreateOperationHandler)
+
+	logger.Info("Server starting on port 8000...")
+
+	if err := http.ListenAndServe(":8000", mux); err != nil {
+		logger.Error("Could not start server", "error", err)
+		os.Exit(1)
+	}
+}
